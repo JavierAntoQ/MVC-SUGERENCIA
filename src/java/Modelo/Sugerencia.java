43d980cd4ca4658d1e5c9a68642ba2e9e9f3fa35
JavierQuispe <1813011329@UNTELS.EDU.PE>
2020-08-03 12:08:28 -0500

    MVC SUGERENCIA MODELO.JAVA TERMINADO

--- src/java/Modelo/Sugerencia.java
+++ src/java/Modelo/Sugerencia.java
@@ -10,5 +10,59 @@ package Modelo;
  * @author USER
  */
 public class Sugerencia {
+    private int idsugerencia;
+    private String nombre;  
+    private String apellido;
+    private String tipo;
+    private String estado;
+
+    public Sugerencia(int idsugerencia, String nombre, String apellido, String tipo, String estado) {
+        this.idsugerencia = idsugerencia;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.tipo = tipo;
+        this.estado = estado;
+    }
+
+    public int getIdsugerencia() {
+        return idsugerencia;
+    }
+
+    public void setIdsugerencia(int idsugerencia) {
+        this.idsugerencia = idsugerencia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
     
 }
